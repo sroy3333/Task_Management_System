731e7febe66c572338fbb1ccbb45ae383bec9215
sroy3333 <121767970+sroy3333@users.noreply.github.com>
2026-08-23 18:34:29 +0530

    Implement TasksController for task management

---
+++ backend/src/tasks/tasks.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  getTasks() {
+    return this.tasksService.getTasks();
+  }
+
+  @Get(':id')
+  getTask(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.getTask(id);
+  }
+
+  @Post()
+  createTask(@Body() body: CreateTaskDto) {
+    return this.tasksService.createTask(body);
+  }
+
+  @Patch(':id')
+  updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateTaskDto,
+  ) {
+    return this.tasksService.updateTask(id, body);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.deleteTask(id);
+  }
+}
